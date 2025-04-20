517e007bb253f47adfbd32e889a1ffdc83ddb032
Burtik21 <simon.stopa@gmail.com>
2025-04-20 16:29:44 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -8,20 +8,21 @@ from app.drivers.pins import Pins
 def create_app():
     app = Flask(__name__)
 
-    calibration = Calibration()
-    deal_card = DealCard()
+
 
     @app.route("/python/deal", methods=["POST"])
     def api_deal():
         data = request.get_json()
         steps = data.get("steps")  # default hodnota
         print(f"📤 API požadavek: vyhodit kartu ({steps} kroků)")
+        deal_card = DealCard()
         deal_card.deal(steps=steps)
         return jsonify({"status": "ok", "message": f"Dealt {steps} steps"})
 
     @app.route("/python/calibrate", methods=["POST"])
     def api_calibrate():
         print("📤 API požadavek: kalibrace")
+        calibration = Calibration()
         calibration.calibration_rotate()
         return jsonify({"status": "ok", "message": "Kalibrace dokončena"})
 
--- app/drivers/calibration.py
+++ app/drivers/calibration.py
@@ -5,21 +5,20 @@ from .step_motor import StepMotor
 from .pins import Pins
 
 class Calibration:
-    def __init__(self,calibration_state=False,calibration_steps=535):  # Nový pin pro nový senzor
+    def __init__(self,calibration_steps=535):  # Nový pin pro nový senzor
 
-        self.calibration_state = calibration_state
+
         self.calibration_steps = calibration_steps
         self.step_motor = StepMotor()
 
     def calibration_rotate(self):
-        if self.calibration_state:
-            return 1
+
 
         print("▶️ Spouštím kalibraci – otáčím krok po kroku...")
         nalezeno = self.step_motor.rotate_until_sensor(max_steps=self.calibration_steps)
 
         if nalezeno:
-            self.calibration_state = True
+
             self.step_motor.stop_motor()
             print("✅ Kalibrace hotová, motor zastaven.")
         else:
