cf5040516a159afbe9729cf4ebbde9967665e218
Burtik21 <simon.stopa@gmail.com>
2025-04-20 17:49:35 +0200

    update

--- app/drivers/step_motor.py
+++ app/drivers/step_motor.py
@@ -86,11 +86,7 @@ class StepMotor:
                     self._actual_steps = 0  # nastavíme výchozí pozici
 
                     # ↪ Potoč se ještě o 50 kroků – ale bez změny stavu
-                    for _ in range(80):
-                        GPIO.output(Pins.MOTOR_STEP_STEP, GPIO.HIGH)
-                        time.sleep(delay)
-                        GPIO.output(Pins.MOTOR_STEP_STEP, GPIO.LOW)
-                        time.sleep(delay)
+
 
                     GPIO.output(Pins.MOTOR_STEP_ENABLE, GPIO.HIGH)
                     return True
