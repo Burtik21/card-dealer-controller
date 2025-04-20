5b6678eb6bf4d16fc918c2b8f9e51c4d6feb8393
Burtik21 <simon.stopa@gmail.com>
2025-04-20 22:40:12 +0200

    update

--- app/drivers/pins.py
+++ app/drivers/pins.py
@@ -20,7 +20,7 @@ class Pins:
     BUTTON_3 = Button(3, 70, 8)
     BUTTON_4 = Button(4, 470, 25)
     BUTTON_5 = Button(5, 390, 24)
-    BUTTON_6 = Button(6, 320, 23)
+    BUTTON_6 = Button(6, 250, 23)
 
     @staticmethod
     def setup_pins():
